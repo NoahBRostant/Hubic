4f909b1d6f80e1b82685b9aee6f7fed433a49fe2
Noah Bradford Rostant <noah.b.rostant@gmail.com>
2021-11-13 18:00:44 +0000

    Added Roundify info & updated the ui

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 window.addEventListener('load', function () {
-    document.getElementById("Content").src="./"+page+".html";
+    document.getElementById("Content").src="./"+page+".html"+"#"+section;
+    if (page=='null') {
+      this.window.URL="./home.html"
+    }
     setTimeout(() => { document.getElementById("Content").style.display="block"; }, 500);
   })
 
@@ -7,3 +10,16 @@ const queryString = window.location.search;
 console.log(queryString);
 const urlParams = new URLSearchParams(queryString);
 const page = urlParams.get('page')
+const section = urlParams.get('section')
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("Topbar").style.top = "20px";
+  } else {
+    document.getElementById("Topbar").style.top = "-60px";
+  }
+  prevScrollpos = currentScrollPos;
+}
