47ee655cd91d5213b0370e17b1f0858134bbb230
Noah Bradford Rostant <noah.b.rostant@gmail.com>
2021-12-02 00:06:55 +0000

    Latest Commit

--- js/darken.js
+++ js/darken.js
@@ -1,49 +1,34 @@
 var x = "0";
-console.log(x)
 
 function DarkMode() {
     if (x == "0") {
         document.body.classList.replace("light-1","dark-1");
         document.getElementById("Topbar").classList.replace("light-2","dark-2");
         document.getElementById("Info-Context").classList.replace("light-2","dark-2");
-        var links = document.getElementsByTagName("a");
-        for(var i=0;i<links.length;i++)
-        {
-            if(links[i].className === "link")
-            {
-                links[i].style.color = "#E5E9F0";
-            }
-        }
-        var divs = document.getElementsByTagName("div");
-        for(var j=0;j<divs.length;j++)
-        {
-            if(divs[j].className === "Line")
-            {
-                divs[j].style.backgroundColor = "#4C566A";
-            }
-        }  
+        document.getElementById("ShieldContainer").classList.replace("light-3","dark-3");
+        document.getElementById("DownloadsTable").classList.replace("light-3","dark-3");
+        document.getElementById("SearchInput").classList.replace("search-light","search-dark");
+        document.getElementById("DownloadsTable").classList.replace("table-light","table-dark");
+        parent.document.getElementById("background").classList.replace("light-1","dark-1");
+        parent.document.getElementById("Sidebar").classList.replace("light-2","dark-2");
         x = "1";
     }
     else {
         document.body.classList.replace("dark-1","light-1");
         document.getElementById("Topbar").classList.replace("dark-2","light-2");
         document.getElementById("Info-Context").classList.replace("dark-2","light-2");
-        var links = document.getElementsByTagName("a");
-        for(var i=0;i<links.length;i++)
-        {
-            if(links[i].className === "link")
-            {
-                links[i].style.color = "#2E3440";
-            }
-        }  
-        var divs = document.getElementsByTagName("div");
-        for(var j=0;j<divs.length;j++)
-        {
-            if(divs[j].className === "Line")
-            {
-                divs[j].style.backgroundcColor = "#3B4252";
-            }
-        }
+        document.getElementById("ShieldContainer").classList.replace("dark-3","light-3");
+        document.getElementById("SearchInput").classList.replace("search-dark","search-light");
+        document.getElementById("DownloadsTable").classList.replace("table-dark","table-light");
+        parent.document.getElementById("background").classList.replace("dark-1","light-1");
+        parent.document.getElementById("Sidebar").classList.replace("dark-2","light-2");
         x = "0";
     }
+}
+
+const hours = new Date().getHours()
+const isDayTime = hours > 6 && hours < 20
+if (isDayTime === false) {
+    setTimeout(() => { document.getElementById('DarkToggle').checked = true;; }, 100);
+    DarkMode();
 }
